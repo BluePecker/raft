afda0c0cc3ddb1bb88dd5e8f39564150b37e3603
shuchao <645904877@qq.com>
2017-05-11 11:07:25 +0800

    add pkg manager

--- raft.go
+++ raft.go
@@ -2,6 +2,7 @@ package raft
 
 import (
     "github.com/BluePecker/raft/types"
+    "github.com/BluePecker/snowflake"
 )
 
 type raft struct {
@@ -11,6 +12,24 @@ type raft struct {
     clock          *types.Clock
     
     member         *types.Nodes
-
+    
     identity       *types.Identity
+}
+
+func NewRafter(NodeId uint64) (*raft, error) {
+    IdWorker, err := snowflake.NewIdWorker(NodeId)
+    if err != nil {
+        return nil, err
+    }
+    
+    UniqueId, err := IdWorker.NextId()
+    if err != nil {
+        return nil, err
+    }
+    
+    return &raft{
+        Term: 0x0,
+        leaderID: 0x0,
+        UniqueID: UniqueId,
+    }, nil
 }
